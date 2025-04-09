52e55c7aafa4e9ea7e271e8656cf8d40629cd43a
Jhonyel Galvis <galvis.j@northeastern.edu>
2025-04-09 18:46:13 -0400

    updates

---
+++ src/frontend/src/pages/ExperiencePage/ExperiencePage.tsx
@@ -0,0 +1,27 @@
+import ErrorPage from "../../components/ErrorPage";
+import LoadingIndicator from "../../components/LoadingIndicator";
+import { useGetAllExperiences } from "../../hooks/experiences.hooks";
+import ExperienceSection from "../../components/ExperienceSection";
+
+function ExperiencesPage() {
+  const {
+    data: experiences,
+    isLoading,
+    isError,
+    error,
+  } = useGetAllExperiences();
+  if (isLoading) {
+    return <LoadingIndicator />;
+  }
+  if (isError || !experiences || error) {
+    return <ErrorPage error={error!} />;
+  }
+  return (
+    <>
+      {experiences.map((experience) => (
+        <ExperienceSection key={experience.id} experience={experience} />
+      ))}
+    </>
+  );
+}
+export default ExperiencesPage;
