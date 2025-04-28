0a3c01547772b29f6915bb5adc7252f9005a7269
Jhonyel Galvis <galvis.j@northeastern.edu>
2025-04-27 22:41:41 -0400

    message

--- src/shared/src/types/experiences.types.ts
+++ src/shared/src/types/experiences.types.ts
@@ -9,3 +9,13 @@ export interface Experience {
   imageUrls: string[];
   creator: User;
 }
+
+export interface ExperiencePreview {
+  id: string;
+  title: string;
+  company: string;
+  location: string;
+  startDate: string; // or Date, depending on your use case
+  endDate: string; // or Date, depending on your use case
+  description: string;
+}
--- src/shared/src/types/projects.types.ts
+++ src/shared/src/types/projects.types.ts
@@ -9,3 +9,14 @@ export interface Project {
   githubUrl: string;
   creator: User;
 }
+
+export interface ProjectPreview {
+  id: string;
+  title: string;
+  description: string;
+  imageUrls: string[];
+  skills: string[];
+  githubUrl: string;
+  createdAt: string; // or Date, depending on your needs
+  updatedAt: string; // or Date, depending on your needs
+}
